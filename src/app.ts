4f6ea8426319dd275e9d8dc0f2e0f4f798ae9b4d
Marcos19842024 <chable240608@hotmail.com>
2025-05-30 11:03:38 -0600

    vf1

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,17 @@ import express from "express"
 import cors from "cors"
 import routes from "./infrastructure/router"
 
+var history = require('connect-history-api-fallback')
 const port = process.env.PORT || 3001
 const path = `${process.cwd()}/`
 const app = express()
 
+app.use(history())
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false, limit: '50mb' }))
-app.use(express.static(path + 'dist/frontend/build/'))
+app.use(express.static(path + 'dist/ecommerce/dist/'))
 app.use(express.static(path + 'tmp'))
-
 app.use(`/`,routes)
 
 app.listen(port, () => console.log(`Ready...${port}`))
